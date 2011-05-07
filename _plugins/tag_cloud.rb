7d2cb6e471bf33012d209e6b96dd1b2df4924a94
Andrey Vityuk <andrey.vityuk@gmail.com>
2011-05-07 21:38:33 +0300

    Cleaned tag cloud plugin

--- _plugins/tag_cloud.rb
+++ _plugins/tag_cloud.rb
@@ -5,20 +5,18 @@ module Jekyll
     def render(context)
       tags = context.registers[:site].tags.map{|tag| 
         { 
-          "title"    => tag[0], 
-          "slug"     => tag[0], 
-          "autoslug" => tag[0], 
-          "posts"    => tag[1] 
+          :tag    => tag[0], 
+          :posts  => tag[1] 
         } 
       }
 
-      min_count = tags.min{|a, b| a["posts"].length <=> b["posts"].length }["posts"].length
-      max_count = tags.max{|a, b| a["posts"].length <=> b["posts"].length }["posts"].length
+      min_count = tags.min{|a, b| a[:posts].length <=> b[:posts].length }[:posts].length
+      max_count = tags.max{|a, b| a[:posts].length <=> b[:posts].length }[:posts].length
 
-      weights = tags.inject({}){|result, tag| result[tag["title"]] = ( ((tag["posts"].length - min_count) * (280 - 75)) / (max_count - min_count) ) + 75; result }
+      weights = tags.inject({}){|result, tag| result[tag[:tag]] = ( ((tag[:posts].length - min_count) * (280 - 75)) / (max_count - min_count) ) + 75; result }
 
       tags.inject("") { |html, tag|
-        html << "<span style='font-size: #{sprintf("%d", weights[tag['title']])}%'><a href='/tags/#{tag['slug'] || tag['autoslug']}/'>#{tag["title"]}</a></span>\n"
+        html << "<span style=\"font-size: #{sprintf("%d", weights[tag[:tag]])}%\"><a href=\"/tags/#{tag[:tag]}/\" rel=\"tag\">#{tag[:tag]}</a></span>\n"
         html
       }
     end
