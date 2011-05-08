c2da05db31d7934d8aa0d4d298dd10ecf85a7321
Andrey Vityuk <andrey.vityuk@gmail.com>
2011-05-08 18:39:51 +0300

    Implemented tags sorting for tag_cloud

--- _plugins/tag_cloud.rb
+++ _plugins/tag_cloud.rb
@@ -12,7 +12,8 @@ module Jekyll
       avg = tags.inject(0.0) {|memo, tag| memo += tag[1].length} / tags.length
       weights = Hash.new
       tags.each {|tag| weights[tag[0]] = tag[1].length/avg}
-      tags.each do |tag, posts|
+      tag_names = tags.keys.sort
+      tag_names.each do |tag|
         html << "<span style=\"font-size: #{sprintf("%d", weights[tag] * 100)}%\"><a href=\"/tags/#{tag}/\">#{tag}</a></span>\n"
       end
       html
