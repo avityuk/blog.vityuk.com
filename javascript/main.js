c941894e811452fce27709063dd74d7744e4122e
Andrey Vityuk <andrey.vityuk@gmail.com>
2011-11-18 22:25:21 -0600

    Added links

--- javascript/main.js
+++ javascript/main.js
@@ -1,19 +1,28 @@
 var config = {
-  feedUrl: "http://feeds.delicious.com/v2/json/avityuk",
-  linksCount: 30
+  feedUrl: "http://feeds.delicious.com/v2/json/avityuk"
 }
 
-function loadLinks() {
+function loadAllLinks() {
+  loadLinks($("#all-links"), 30);
+}
+
+function loadSidebarLinks() {
+  loadLinks($("#links-list"), 5);
+}
+
+function loadLinks(element, maxLinks) {
   $.ajax({
     url: config.feedUrl,
     dataType: "jsonp",
     data: {
-      count: config.linksCount
+      count: maxLinks
     },
     success: function(data) {
+      var links = $("<ul class='links-list'></ul>");
       $(data).each(function(i, v) {
-        $("#my_links").append("<li><a href='" + v.u + "'>" + v.d + "</a></li>");
+        links.append("<li><a href='" + v.u + "'>" + v.d + "</a></li>");
       });
+      element.prepend(links);
     }
   });
 }
